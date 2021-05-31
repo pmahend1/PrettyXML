1616dae01c84e574d882e6e2927a7177e5b058f2
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2021-05-30 23:00:33 -0400

    Mac fix (#69)

--- lib/electron-edge-js-mac/build/Release/.deps/Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o.d
+++ lib/electron-edge-js-mac/build/Release/.deps/Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o.d
@@ -1,24 +1,24 @@
-cmd_Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o := c++ -o Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o ../src/mono/nodejsfunc.cpp '-DNODE_GYP_MODULE_NAME=edge_nativeclr' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DV8_COMPRESS_POINTERS' '-DV8_31BIT_SMIS_ON_64BIT_ARCH' '-DV8_REVERSE_JSARGS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/src -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/deps/openssl/config -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/deps/openssl/openssl/include -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/deps/uv/include -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/deps/zlib -I/Users/prateekmahendrakar/.electron-gyp/11.2.1/deps/v8/include -I../node_modules/nan -I/Library/Frameworks/Mono.framework/Versions/Current/include/mono-2.0 -I/Library/Frameworks/Mono.framework/Versions/6.12.0/include/glib-2.0 -I/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/glib-2.0/include -I/Library/Frameworks/Mono.framework/Versions/6.12.0/include  -O3 -gdwarf-2 -mmacosx-version-min=10.10 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -DHAVE_NATIVECLR -MMD -MF ./Release/.deps/Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o.d.raw   -c
+cmd_Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o := c++ -o Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o ../src/mono/nodejsfunc.cpp '-DNODE_GYP_MODULE_NAME=edge_nativeclr' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DV8_COMPRESS_POINTERS' '-DV8_31BIT_SMIS_ON_64BIT_ARCH' '-DV8_REVERSE_JSARGS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/src -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/deps/openssl/config -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/deps/openssl/openssl/include -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/deps/uv/include -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/deps/zlib -I/Users/prateekmahendrakar/.electron-gyp/12.0.4/deps/v8/include -I../node_modules/nan -I/Library/Frameworks/Mono.framework/Versions/Current/include/mono-2.0 -I/Library/Frameworks/Mono.framework/Versions/6.12.0/include/glib-2.0 -I/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/glib-2.0/include -I/Library/Frameworks/Mono.framework/Versions/6.12.0/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -DHAVE_NATIVECLR -MMD -MF ./Release/.deps/Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o.d.raw   -c
 Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o: \
   ../src/mono/nodejsfunc.cpp ../src/mono/edge.h \
   ../src/mono/../common/edge_common.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/cppgc/common.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8config.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8-internal.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8-version.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8-platform.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node_version.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node_buffer.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/errno.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/version.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/unix.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/threadpool.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/darwin.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/cppgc/common.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8config.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8-internal.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8-version.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8-platform.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node_version.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node_buffer.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/errno.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/version.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/unix.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/threadpool.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/darwin.h \
   ../node_modules/nan/nan.h \
-  /Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node_object_wrap.h \
+  /Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node_object_wrap.h \
   ../node_modules/nan/nan_callbacks.h \
   ../node_modules/nan/nan_callbacks_12_inl.h \
   ../node_modules/nan/nan_maybe_43_inl.h \
@@ -47,23 +47,23 @@ Release/obj.target/edge_nativeclr/src/mono/nodejsfunc.o: \
 ../src/mono/nodejsfunc.cpp:
 ../src/mono/edge.h:
 ../src/mono/../common/edge_common.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/cppgc/common.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8config.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8-internal.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8-version.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/v8-platform.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node_version.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node_buffer.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/errno.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/version.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/unix.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/threadpool.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/uv/darwin.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/cppgc/common.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8config.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8-internal.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8-version.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/v8-platform.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node_version.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node_buffer.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/errno.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/version.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/unix.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/threadpool.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/uv/darwin.h:
 ../node_modules/nan/nan.h:
-/Users/prateekmahendrakar/.electron-gyp/11.2.1/include/node/node_object_wrap.h:
+/Users/prateekmahendrakar/.electron-gyp/12.0.4/include/node/node_object_wrap.h:
 ../node_modules/nan/nan_callbacks.h:
 ../node_modules/nan/nan_callbacks_12_inl.h:
 ../node_modules/nan/nan_maybe_43_inl.h:
