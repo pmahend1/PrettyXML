3cce8ddce85a4e7cc1a74817726bf78c1c00c213
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2021-08-08 22:35:13 -0400

    electron rebuild for 13.1.7 - mac

--- lib/electron-edge-js-mac/build/Release/.deps/lib/bootstrap/project.lock.json.d
+++
@@ -1,1 +0,0 @@
-cmd_lib/bootstrap/project.lock.json := LD_LIBRARY_PATH=/Users/prateekmahendrakar/Projects/electron-edge-js-mac/build/Release/lib.host:/Users/prateekmahendrakar/Projects/electron-edge-js-mac/build/Release/lib.target:$$LD_LIBRARY_PATH; export LD_LIBRARY_PATH; cd ../.; mkdir -p lib/bootstrap; bash -c "cd lib/bootstrap && dotnet restore"
