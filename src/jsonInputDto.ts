46b77fcb17e27e8a9c9ff319218e27b450d49a2d
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2021-09-17 23:53:14 -0400

    added command line support

---
+++ src/jsonInputDto.ts
@@ -0,0 +1,20 @@
+import { FormattingActionKind } from "./formattingActionKind";
+import { Settings } from "./settings";
+
+export class JsonInputDto
+{
+
+    xmlString: string;
+
+    formattingOptions: Settings;
+
+    actionKind: FormattingActionKind;
+
+    constructor(xmlString: string, actionKind: FormattingActionKind, formattingOptions?: Settings)
+    {
+        this.xmlString = xmlString;
+        this.actionKind = actionKind;
+        this.formattingOptions = formattingOptions ?? new Settings();
+    }
+
+}
