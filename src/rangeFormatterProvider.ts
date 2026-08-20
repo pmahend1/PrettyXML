c040fb7801e6c149d4176ca0e913e980c66f9583
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2026-08-19 23:33:57 -0400

    Fixes whitespace and unicode support

--- src/rangeFormatterProvider.ts
+++ src/rangeFormatterProvider.ts
@@ -1,34 +1,52 @@
 import { CancellationToken, DocumentRangeFormattingEditProvider, FormattingOptions, ProviderResult, Range, TextDocument, TextEdit } from "vscode";
+import { Formatter } from "./formatter";
 import { TextXmlFormatter } from "./regexFormatter";
-import {  Settings } from "./settings";
+import { defaultSettings, Settings } from "./settings";
 
 export class RangeFormatterProvider implements DocumentRangeFormattingEditProvider {
 
-    private settings: Settings;
-    constructor(settings: Settings) {
-        this.settings = settings;
+    private formatter?: Formatter;
+    private settings?: Settings;
+
+    constructor(settingsOrFormatter: Settings | Formatter) {
+        if (settingsOrFormatter instanceof Formatter) {
+            this.formatter = settingsOrFormatter;
+        } else {
+            this.settings = settingsOrFormatter;
+        }
     }
+
+    private getSettings(): Settings {
+        if (this.formatter) {
+            this.formatter.loadSettings();
+            return this.formatter.settings;
+        }
+        return this.settings ?? defaultSettings;
+    }
+
     provideDocumentRangeFormattingEdits(document: TextDocument, range: Range, options: FormattingOptions, token: CancellationToken): ProviderResult<TextEdit[]> {
         if (document) {
-            var text = document.getText(range);
+            const text = document.getText(range);
             if (text) {
-                var regexFormatter = new TextXmlFormatter(this.settings);
-                var formattedText = regexFormatter.formatXmlPretty(text);
+                const regexFormatter = new TextXmlFormatter(this.getSettings());
+                const formattedText = regexFormatter.formatXmlPretty(text);
                 const replacer = TextEdit.replace(range, formattedText);
-                return [replacer]
+                return [replacer];
             }
         }
         return [];
     }
+
     provideDocumentRangesFormattingEdits?(document: TextDocument, ranges: Range[], options: FormattingOptions, token: CancellationToken): ProviderResult<TextEdit[]> {
         if (document) {
-            var edits = new Array<TextEdit>();
+            const currentSettings = this.getSettings();
+            const edits = new Array<TextEdit>();
             for (let i = 0; i < ranges.length; i++) {
                 const range = ranges[i];
-                var text = document.getText(range);
+                const text = document.getText(range);
                 if (text) {
-                    var regexFormatter = new TextXmlFormatter(this.settings);
-                    var formattedText = regexFormatter.formatXmlPretty(text);
+                    const regexFormatter = new TextXmlFormatter(currentSettings);
+                    const formattedText = regexFormatter.formatXmlPretty(text);
                     const replacer = TextEdit.replace(range, formattedText);
                     edits.push(replacer);
                 }
